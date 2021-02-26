24556c046fb4bdf15f88aac08089808f3ac88926
MusaPolka <75001112+MusaPolka@users.noreply.github.com>
2021-02-26 11:02:51 +0600

    Add files via upload

---
+++ p-18-44.kz/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+/* $x = 70;
+$y = 35;
+$z = $x / $y;
+echo $z. '<br>';
+
+$q = 10;
+$num = $q * $z;
+
+echo $num. '<br>';
+
+$age = 17;
+echo 'Менын жасым '.$age . '<br>';
+
+$str = "qwerty";
+
+echo $str[0] . ' ' . $str[1] . ' ' . $str[2] . '<br>'; */
+
+$arr[0]=24;
+$arr[1]=47;
+$arr[2]=6;
+$arr[3]=13;
+$arr[4]=5;
+$arr[5]=89;
+
+$result = ($arr[5] - $arr[1]) + ($arr[0] - $arr[3]) + ($arr[1] - $arr[2]);
+
+echo $result;
+
+
+    ?>
+</body>
+</html>
